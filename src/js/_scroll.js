eae5d43cb97575d2f18c382593f80d0f8e2b155c
Kalinovsky <40641362+microfrontender@users.noreply.github.com>
2022-02-07 19:08:44 +0700

    update

---
+++ src/js/_scroll.js
@@ -0,0 +1,64 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+
+export default function Scroller() {
+
+
+	const scroll = new LocomotiveScroll({
+		el: document.querySelector('[data-scroll-container]'),
+		smooth: true,
+		direction: 'horizontal',
+	});
+
+	let spines = document.querySelectorAll('.spine[data-spine-section]');
+
+
+	scroll.on('scroll', (args) => {
+
+		spines.forEach((spine, index) => {
+			let attr = spine.getAttribute('data-spine-section');
+			let section = document.querySelector(`section[data-spine-section='${attr}']`);
+
+			if (section.getBoundingClientRect().left < window.innerWidth && section.getBoundingClientRect().left > 0) {
+
+				if (section.getBoundingClientRect().left <= spine.offsetWidth * attr) {
+					spine.classList.add('fixed');
+					spine.classList.remove('active');
+					spine.style.left = '';
+				} else if (section.getBoundingClientRect().left >= window.innerWidth - spine.offsetWidth * (5 - attr)) {
+					spine.classList.add('init');
+					spine.classList.remove('active');
+					spine.style.left = '';
+				} else {
+					spine.classList.add('active');
+					spine.classList.remove('init');
+					spine.classList.remove('fixed');
+				}
+
+				if (spine.classList.contains('active')) {
+					spine.style.left = section.getBoundingClientRect().left + 'px';
+				}
+
+
+
+			}
+		});
+	});
+
+
+	scroll.scrollTo(document.querySelector('#section-1'),{
+		duration: 10,
+		offset: -64
+	});
+	// function jump(h){
+	// 	let url = location.href;              
+	// 	let spine = document.querySelector(`.spine[data-spine-section='${h}']`);
+	// 	console.log(spine);
+	// 	location.href = "#section-"+h;         
+	// 	history.replaceState(null,null,url);  
+	// 	spine.classList = 'spine fixed';
+	// }
+	// jump('1');
+
+	// document.querySelector('#section-2').scrollIntoView(true);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
-/* Code by Kalinovsky.
-    Find me here
-    mail - kalinovsky.job@gmail.com
-    tg - @AlexKalinovsky 
-    */
+
 
 import '../sass/index.sass';
+import Scroller from './_scroll';
+
+Scroller();
